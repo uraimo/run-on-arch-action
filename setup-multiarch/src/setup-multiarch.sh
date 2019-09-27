b57732ca47821ad60dab8fc87c2dc6f59735eeff
Umberto Raimondi <me@uraimo.com>
2019-09-27 20:53:48 +0200

    Test multiple command execution

--- setup-multiarch/src/setup-multiarch.sh
+++ setup-multiarch/src/setup-multiarch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo apt update -y && sudo apt install -y qemu qemu-user-static
+# Install support for new archs via qemu
+# Platforms: linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
