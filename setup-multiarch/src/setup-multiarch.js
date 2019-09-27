4c9335643695d145211bfac386c39e9629e3b528
Umberto Raimondi <me@uraimo.com>
2019-09-27 20:23:02 +0200

    Fixed Linux check

--- setup-multiarch/src/setup-multiarch.js
+++ setup-multiarch/src/setup-multiarch.js
@@ -2,7 +2,7 @@ const core = require('@actions/core')
 const {exec} = require('@actions/exec')
 
 async function main() {
-  if (process.platform !== 'linux') {
+  if (process.platform === 'linux') {
     console.log('Configuring Docker for multi-architecture support')
     await exec('./setup-multiarch.sh')
   } else {
