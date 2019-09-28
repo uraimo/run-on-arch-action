37be3d8365751f0969d3a1603c3f4ab39b15e648
Umberto Raimondi <me@uraimo.com>
2019-09-28 17:54:43 +0200

    Fix git cloning

--- src/run-on-arch.sh
+++ src/run-on-arch.sh
@@ -8,12 +8,9 @@ sudo apt update -y && sudo apt install -y qemu qemu-user-static
 # Install support for new archs via qemu
 # Platforms: linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
 
-git clone https://github.com/uraimo/multiarch-actions.git
-
-pwd
-ls -al
+ln -s /home/runner/work/_actions/uraimo/multiarch-actions/master/ multi-act
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build . --file multiarch-actions/Dockerfiles/Dockerfile.$ARCH.$DISTRO --tag multiarchimage
+docker build . --file multi-act/Dockerfiles/Dockerfile.$ARCH.$DISTRO --tag multiarchimage
 
-docker run --workdir /github/workspace --rm -e HOME -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e RUNNER_OS -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -v "/var/run/docker.sock":"/var/run/docker.sock" -v "/home/runner/work/_temp/_github_home":"/github/home" -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" -v "/home/runner/work/multiarch-actions/multiarch-actions":"/github/workspace" -t multiarchimage "$RUNS" 
+docker run --workdir /github/workspace --rm -e HOME -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e RUNNER_OS -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -v "/var/run/docker.sock":"/var/run/docker.sock" -v "/home/runner/work/_temp/_github_home":"/github/home" -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" -v "/home/runner/work/GITHUB_REPOSITORY/GITHUB_REPOSITORY":"/github/workspace" -t multiarchimage "$RUNS" 
