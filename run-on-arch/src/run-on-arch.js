e1b334d2f237c818924b0fecfafb6400e83422c2
Umberto Raimondi <me@uraimo.com>
2019-09-28 16:45:48 +0200

    Fix typo

--- run-on-arch/src/run-on-arch.js
+++ run-on-arch/src/run-on-arch.js
@@ -7,7 +7,7 @@ async function main() {
 
     const arch = core.getInput('architecture', {required: true})
     const distro = core.getInput('distribution', {required: true})
-    const runs = core.getInput('runs', {required: true})
+    const runs = core.getInput('run', {required: true})
 
     console.log('Configuring Docker for multi-architecture support')
     await exec(path.join(__dirname, 'run-on-arch.sh'),[arch,distro,runs])
