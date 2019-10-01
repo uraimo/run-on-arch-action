b7ce2a6f310d5525134faabaf69a69870ead2032
Umberto Raimondi <me@uraimo.com>
2019-10-01 20:53:13 +0200

    Fix branch discovery, for real

--- src/run-on-arch.sh
+++ src/run-on-arch.sh
@@ -9,10 +9,9 @@ COMMANDS="${COMMANDS//[$'\t\r\n']/;}" #Replace newline with ;
 # Platforms: linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
 sudo apt update -y && sudo apt install -y qemu qemu-user-static
 
-# Symbolic link for the action repository location
-ln -s /home/runner/work/_actions/uraimo/run-on-arch-action/${GITHUB_REF##*/}/ multi-act
+ACT_PATH = $(dirname $(dirname "$0"))
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build . --file multi-act/Dockerfiles/Dockerfile.$ARCH.$DISTRO --tag multiarchimage 
+docker build . --file $ACT_PATH/Dockerfiles/Dockerfile.$ARCH.$DISTRO --tag multiarchimage 
 
 docker run --workdir /github/workspace --rm -e HOME -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e RUNNER_OS -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -v "/var/run/docker.sock":"/var/run/docker.sock" -v "/home/runner/work/_temp/_github_home":"/github/home" -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" -v "${PWD}":"/github/workspace" -t multiarchimage /bin/bash -c "$COMMANDS"
