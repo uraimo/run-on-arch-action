af230f8ec20d0dd55048d2a4f05178351d44df90
Elijah Shaw-Rutschman <elijahr@gmail.com>
2020-09-01 03:42:07 -0500

    New input parameters, various improvements
    * `githubToken`: Your GitHub token, used for caching Docker images in your project's public package registry. Usually this would just be `${{ github.token }}`. This speeds up builds and is highly recommended.
    * `env`: Environment variables to propagate to the container. YAML, but must begin with a | character. These variables will be available in both run and setup.
    * `shell`: The shell to run commands with in the container. Default: `/bin/sh` on Alpine, `/bin/bash` for other distros.
    * `dockerRunArgs`: Additional arguments to pass to `docker run`, such as volume mappings. See [`docker run` documentation](https://docs.docker.com/engine/reference/commandline/run).
    * `setup`: Shell commands to execute on the host before running the container, such as creating directories for volume mappings.
    
    Other changes:
    
    * Rename `additionalArgs` -> `dockerRunArgs` for clarity
    * Rename `architecture` -> `arch`, `distribution` -> `distro` for brevity
    * Add unit tests for all the inputs, run unit tests in a matrix against multiple distros/archs
    * Fix Alpine (`/bin/bash` doesn't exist because BusyBox, must use `/bin/sh`)
    * Improved docs and updated examples
    * Passing the `run` commands inline to `docker run` was causing issues depending on the script's contents. That is fixed by instead placing `run` commands in a script file.

--- src/run-on-arch.js
+++ src/run-on-arch.js
@@ -1,20 +1,94 @@
 const core = require('@actions/core')
+const fs = require('fs');
 const path = require('path')
-const {exec} = require('@actions/exec')
+const YAML = require('yaml');
+const { exec } = require('@actions/exec')
+
+function slug(str) {
+  return str.replace(/[^a-zA-Z0-9]/g, '-');
+}
 
 async function main() {
-  if (process.platform === 'linux') {
-
-    const arch = core.getInput('architecture', {required: true})
-    const distro = core.getInput('distribution', {required: true})
-    const runs = core.getInput('run', {required: true})
-    const additionalArgs = core.getInput('additionalArgs')
-
-    console.log('Configuring Docker for multi-architecture support')
-    await exec(path.join(__dirname, 'run-on-arch.sh'),[arch,distro,runs,additionalArgs])
-  } else {
+  if (process.platform !== 'linux') {
     throw new Error('run-on-arch supports only Linux')
   }
+
+  const arch = core.getInput('arch', { required: true });
+  const distro = core.getInput('distro', { required: true });
+
+  // If bad arch/distro passed, fail fast before installing all the qemu stuff
+  const dockerFile = path.join(
+    __dirname, '..', 'Dockerfiles', `Dockerfile.${arch}.${distro}`);
+  if (!fs.existsSync(dockerFile)) {
+    throw new Error(`run-on-arch: ${dockerFile} does not exist.`);
+  }
+
+  // Write setup commands to a script file for sourcing
+  let setup = core.getInput('setup') || '';
+  fs.writeFileSync(
+    path.join(__dirname, 'run-on-arch-setup.sh'),
+    setup,
+  );
+
+  // If no shell provided, default to sh for alpine, bash for others
+  let shell = core.getInput('shell') || '';
+  if (!shell) {
+    if (/alpine/.test(distro)) {
+      shell = '/bin/sh';
+    } else {
+      shell = '/bin/bash';
+    }
+  }
+
+  // Write container commands to a script file for running
+  let commands = core.getInput('run', { required: true });
+  commands = `#!${shell}\nset -eu\n${commands}`;
+  fs.writeFileSync(
+    path.join(__dirname, 'run-on-arch-commands.sh'),
+    commands,
+  );
+
+  // Sanitize dockerRunArgs: replace escaped and unescaped newlines with a space
+  let dockerRunArgs = (core.getInput('dockerRunArgs') || '')
+    .replace(/\\?[\r\n]+/g, ' ');
+
+  const githubToken = core.getInput('githubToken') || '';
+
+  // Copy environment variables from parent process
+  const env = { ...process.env };
+
+  if (githubToken) {
+    env.GITHUB_TOKEN = githubToken;
+  }
+
+  // Parse YAML and for environment variables.
+  // They are imported to the container via passing `-e VARNAME` to
+  // docker run.
+  const envYAML = core.getInput('env');
+  if (envYAML) {
+    const mapping = YAML.parse(envYAML)
+    if (typeof mapping !== 'object' || mapping instanceof Array) {
+      throw new Error(`run-on-arch: env must be a flat mapping of key/value pairs.`);
+    }
+    Object.entries(mapping).forEach(([key, value]) => {
+      if (typeof value === 'object') {
+        // Nested YAML is invalid
+        throw new Error(`run-on-arch: env ${key} value must be flat.`);
+      }
+      env[key] = value;
+      dockerRunArgs += ` -e ${key} `;
+    });
+  }
+
+  // Generate a container name slug unique to this arch/distro combo
+  const containerName = `run-on-arch-${slug(arch)}-${slug(distro)}`;
+
+  console.log('Configuring Docker for multi-architecture support')
+  await exec(
+    path.join(__dirname, 'run-on-arch.sh'),
+    [ dockerFile, dockerRunArgs, containerName, shell],
+    { env },
+  );
 }
 
 main().catch(err => {
--- src/run-on-arch.sh
+++ src/run-on-arch.sh
@@ -1,46 +1,136 @@
 #!/bin/bash
 
-set -eo pipefail
+set -euo pipefail
 
-ARCH=$1
-DISTRO=$2
-COMMANDS=$3
-COMMANDS="${COMMANDS//[$'\t\r\n']+/;}" #Replace newline with ;
-ADDITIONAL_ARGS=$4
+# Args
+DOCKERFILE=$1
+DOCKER_RUN_ARGS=$2
+CONTAINER_NAME=$3
+
+# Defaults
+ACTION_DIR="$(cd "$(dirname "$0")"/.. >/dev/null 2>&1 ; pwd -P)"
+GITHUB_TOKEN=${GITHUB_TOKEN:-}
+PACKAGE_REGISTRY="docker.pkg.github.com/${GITHUB_REPOSITORY}/${CONTAINER_NAME}"
 DEBIAN_FRONTEND=noninteractive
 
-# Install support for new archs via qemu
-# Platforms: linux/amd64, linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/arm/v7, linux/arm/v6
-sudo apt-get update -q -y && sudo apt-get install -q -y qemu qemu-user-static
+show_build_log_and_exit () {
+  # Show build-log.text output and exit if passed exit status != 0
+  status=$1
+  if [[ "$status" != 0 ]]
+  then
+    cat build-log.txt
+    exit $status
+  fi
+}
 
-ACT_PATH=$(dirname $(dirname $(readlink -fm "$0")))
+quiet () {
+  # Hide the output of some command, unless it fails.
+  # If it fails, output is echoed and this script exits with the command's
+  # exit status code.
+  eval "$@" >> build-log.txt 2>&1 || show_build_log_and_exit $?
+}
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build . --file $ACT_PATH/Dockerfiles/Dockerfile.$ARCH.$DISTRO --tag multiarchimage 
+install_deps () {
+  # Install support for non-amd64 emulation in Docker via QEMU.
+  # Platforms: linux/arm64, linux/riscv64, linux/ppc64le, linux/s390x,
+  #            linux/386, linux/arm/v7, linux/arm/v6
+  sudo apt-get update -y
+  sudo apt-get -qq install -y qemu qemu-user-static
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+}
 
-docker run \
-  --workdir /github/workspace \
-  --rm \
-  -e HOME=/github/home \
-  -e GITHUB_REF \
-  -e GITHUB_SHA \
-  -e GITHUB_REPOSITORY \
-  -e GITHUB_ACTOR \
-  -e GITHUB_WORKFLOW=/github/workflow \
-  -e GITHUB_HEAD_REF \
-  -e GITHUB_BASE_REF \
-  -e GITHUB_EVENT_NAME \
-  -e GITHUB_WORKSPACE=/github/workspace \
-  -e GITHUB_ACTION \
-  -e GITHUB_EVENT_PATH \
-  -e RUNNER_OS \
-  -e RUNNER_TOOL_CACHE \
-  -e RUNNER_TEMP \
-  -e RUNNER_WORKSPACE \
-  -v "/var/run/docker.sock":"/var/run/docker.sock" \
-  -v "/home/runner/work/_temp/_github_home":"/github/home" \
-  -v "/home/runner/work/_temp/_github_workflow":"/github/workflow" \
-  -v "${PWD}":"/github/workspace" \
-  $ADDITIONAL_ARGS \
-  -t multiarchimage \
-  /bin/bash -c "$COMMANDS"
+build_container () {
+  # Build the container image.
+
+  # If the GITHUB_TOKEN env var has a value, the container images will be
+  # cached between builds.
+  if [[ -z "$GITHUB_TOKEN" ]]
+  then
+    docker build . --file "$DOCKERFILE" --tag "${CONTAINER_NAME}:latest"
+  else
+    # Build optimization that uses GitHub package registry to cache docker
+    # images, based on Thai Pangsakulyanont's experiments.
+    # Read about it: https://dev.to/dtinth/caching-docker-builds-in-github-actions-which-approach-is-the-fastest-a-research-18ei
+    # Implementation is `build_with_gpr` here: https://github.com/dtinth/github-actions-docker-layer-caching-poc/blob/master/.github/workflows/dockerimage.yml
+    # About GitHub package registry: https://docs.github.com/en/packages/publishing-and-managing-packages/about-github-packages#support-for-package-registries
+    echo "GitHub token provided, caching to $PACKAGE_REGISTRY"
+
+    # Login without echoing token, just in case
+    BASH_FLAGS="$-"
+    set +x
+    echo "$GITHUB_TOKEN" | docker login docker.pkg.github.com \
+      -u "$GITHUB_ACTOR" \
+      --password-stdin
+    set "$BASH_FLAGS"
+
+    docker pull "$PACKAGE_REGISTRY:latest" || true
+    docker build . --file "$DOCKERFILE" \
+      --tag "${CONTAINER_NAME}:latest" \
+      --cache-from="$PACKAGE_REGISTRY"
+    docker tag "${CONTAINER_NAME}:latest" "$PACKAGE_REGISTRY" \
+      && docker push "$PACKAGE_REGISTRY" || true
+  fi
+}
+
+run_container () {
+  # Run the container.
+
+  # Run user-provided setup script, in same shell
+  source "${ACTION_DIR}/src/run-on-arch-setup.sh"
+
+   # Interpolate DOCKER_RUN_ARGS, to support evaluation of $VAR references
+  DOCKER_RUN_ARGS=$(eval echo "$DOCKER_RUN_ARGS")
+
+  chmod +x "${ACTION_DIR}/src/run-on-arch-commands.sh"
+
+  # The location of the event.json file
+  EVENT_DIR=$(dirname "$GITHUB_EVENT_PATH")
+
+  docker run \
+    --workdir "${GITHUB_WORKSPACE}" \
+    --rm \
+    -e DEBIAN_FRONTEND=noninteractive \
+    -e CI \
+    -e GITHUB_WORKFLOW \
+    -e GITHUB_RUN_ID \
+    -e GITHUB_RUN_NUMBER \
+    -e GITHUB_ACTION \
+    -e GITHUB_ACTIONS \
+    -e GITHUB_ACTOR \
+    -e GITHUB_REPOSITORY \
+    -e GITHUB_EVENT_NAME \
+    -e GITHUB_EVENT_PATH \
+    -e GITHUB_WORKSPACE \
+    -e GITHUB_SHA \
+    -e GITHUB_REF \
+    -e GITHUB_HEAD_REF \
+    -e GITHUB_BASE_REF \
+    -e GITHUB_SERVER_URL \
+    -e GITHUB_API_URL \
+    -e GITHUB_GRAPHQL_URL \
+    -e GITHUB_REF \
+    -e GITHUB_SHA \
+    -e GITHUB_HEAD_REF \
+    -e GITHUB_BASE_REF \
+    -e GITHUB_EVENT_NAME \
+    -e GITHUB_WORKSPACE \
+    -e GITHUB_EVENT_PATH \
+    -e RUNNER_TOOL_CACHE \
+    -e RUNNER_TEMP \
+    -e RUNNER_WORKSPACE \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    -v "${EVENT_DIR}:${EVENT_DIR}" \
+    -v "${GITHUB_WORKSPACE}:${GITHUB_WORKSPACE}" \
+    -v "${ACTION_DIR}:${ACTION_DIR}" \
+    --tty \
+    $DOCKER_RUN_ARGS \
+    "${CONTAINER_NAME}:latest" \
+    "${ACTION_DIR}/src/run-on-arch-commands.sh"
+}
+
+# Installing deps produces a lot of log noise, so we do so quietly
+quiet rm -f build-log.txt
+quiet install_deps
+
+build_container
+run_container
